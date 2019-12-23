e554cc01b1c0ce77a1a780b26462a82ce1b004d0
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-12-23 03:00:40 +0000

    wip: events api receive event

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from starlette.config import Config
 from starlette.routing import Route
+from starlette.requests import Request
 from starlette.datastructures import Secret
 from starlette.middleware import Middleware
+from starlette.responses import PlainTextResponse
 from starlette.responses import JSONResponse
 from starlette.applications import Starlette
 
@@ -11,12 +13,22 @@ config = Config("deploy.env")
 DEBUG = config("DEBUG", cast=bool, default=False)
 
 
-async def homepage(request):
+async def homepage(request: Request):
     return JSONResponse({"hello": "world"})
 
 
+async def mention(request: Request):
+    print(request.state.json)
+    return PlainTextResponse("helloooo!")
+
+
+routes = [
+    Route("/", homepage),
+    Route("/events", endpoint=mention, methods=["POST"]),
+]
+
 middleware = [Middleware(VerifySlackSignature)]
-app = Starlette(debug=DEBUG, routes=[Route("/", homepage),], middleware=middleware)
+app = Starlette(debug=DEBUG, routes=routes, middleware=middleware)
 app.state.SLACK_API_TOKEN = config("SLACK_API_TOKEN", cast=Secret)
 app.state.SLACK_SIGNING_SECRET = config("SLACK_SIGNING_SECRET", cast=Secret)
 
--- app/middlewares.py
+++ app/middlewares.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import hmac
+import json
 import hashlib
 from time import time
 from functools import wraps
@@ -11,25 +12,24 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 
 class VerifySlackSignature(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
+    async def dispatch(self, request: Request, call_next):
+        if "/events" in request.url.path:
+            timestamp = request.headers.get("X-Slack-Request-Timestamp")
+            if abs(time() - int(timestamp)) > 60 * 5:
+                return Response("Invalid request timestamp.", status_code=403)
 
-        timestamp = request.headers.get("X-Slack-Request-Timestamp")
-        if abs(time() - int(timestamp)) > 60 * 5:
-            return Response("Invalid request timestamp", status_code=403)
+            req_bytes = await request.body()
+            request.state.json = json.loads(req_bytes)
 
-        signature = request.headers.get("X-Slack-Signature")
-        req = str.encode("v0:" + str(timestamp) + ":") + request.body()
-        request_hash = (
-            "v0="
-            + hmac.new(str.encode(SIGNING_SECRET), req, hashlib.sha256).hexdigest()
-        )
-        # Compare byte strings for Python 2
-        if sys.version_info[0] == 2:
-            return hmac.compare_digest(bytes(request_hash), bytes(signature))
-        else:
-            return hmac.compare_digest(request_hash, signature)
+            signature = request.headers.get("X-Slack-Signature")
+            req = str.encode("v0:" + str(timestamp) + ":") + req_bytes
+            SECRET = str(request.app.state.SLACK_SIGNING_SECRET)
+            rhash = (
+                "v0=" + hmac.new(str.encode(SECRET), req, hashlib.sha256).hexdigest()
+            )
 
-        response = await call_next(request)
-        response.headers["Custom"] = "Example"
-        return response
+            if not hmac.compare_digest(rhash, signature):
+                return Response("Invalid request.", status_code=403)
+            print("Signature valid.")
+        return await call_next(request)
 
