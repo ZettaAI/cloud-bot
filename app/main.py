4c6a0cdaee2afbbb497b9672ade423874e9a778b
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-12-29 16:50:12 +0000

    wip: workers

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import sys
+
+import pika
 from starlette.config import Config
 from starlette.routing import Route
 from starlette.requests import Request
@@ -13,8 +16,18 @@ config = Config("deploy.env")
 DEBUG = config("DEBUG", cast=bool, default=False)
 
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+channel.exchange_declare(exchange="topic_logs", exchange_type="topic")
+
+routing_key = sys.argv[1] if len(sys.argv) > 2 else "anonymous.info"
+message = " ".join(sys.argv[2:]) or "Hello World!"
+channel.basic_publish(exchange="topic_logs", routing_key=routing_key, body=message)
+connection.close()
+
+
 async def homepage(request: Request):
-    return JSONResponse({"hello": "world"})
+    return PlainTextResponse("helloooo!")
 
 
 async def mention(request: Request):
