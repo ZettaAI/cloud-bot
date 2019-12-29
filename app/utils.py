5e4a2182d87a51e18984fae7e4055cb1e8dfe2a8
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-12-29 21:18:18 +0000

    request verifcation docs

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,13 @@ from starlette.requests import Request
 
 
 def verify_request(request: Request, req_bytes: bytes) -> bool:
+    """
+    See https://api.slack.com/docs/verifying-requests-from-slack
+    
+    The request body is stored in request state because await can only be called once.
+    JSON loads and dumps are not guaranteed to produce same strings
+    so passing `req_bytes` is necessary.
+    """
     timestamp = request.headers.get("X-Slack-Request-Timestamp")
     signature = request.headers.get("X-Slack-Signature")
     req = str.encode(f"v0:{str(timestamp)}:") + req_bytes
