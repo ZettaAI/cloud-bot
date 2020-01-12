99f7d9dadf8adffa2a01bbf7a3eb123f815c0f77
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-12 20:00:51 +0000

    remove slack formatting

--- app/messaging.py
+++ app/messaging.py
@@ -6,6 +6,7 @@ from .slack import EventTypes
 
 from .config import AMQP_CREDS
 from .config import AMQP_SERVICE_HOST
+from .utils import parse_slack_message
 
 
 def get_ampq_connection():
@@ -25,11 +26,11 @@ def publish(request):
         event = request.state.json["event"]
         cmd_txt = event["text"]
     except KeyError:
-        print(dumps(event, indent=2))
         return
     cmd_txt = (
         cmd_txt.split(" ", 1) if event["type"] == EventTypes.APP_MENTION else cmd_txt
     )
+    cmd_txt = parse_slack_message(cmd_txt)
     r_key = f"{cmd_txt.split()[0]}.#"
     channel.exchange_declare(exchange="cloud_bot", exchange_type="topic")
 
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,8 @@
+import re
 import hmac
 from json import dumps
 from hashlib import sha256
+
 from starlette.requests import Request
 
 
@@ -18,3 +20,14 @@ def verify_request(request: Request, req_bytes: bytes) -> bool:
     SECRET = str(request.app.state.SLACK_SIGNING_SECRET)
     rhash = "v0=" + hmac.new(str.encode(SECRET), req, sha256).hexdigest()
     return hmac.compare_digest(rhash, signature)
+
+
+def parse_slack_message(msg: str) -> str:
+    """
+    Slack formats some of the text like emails and URLs with `<>`.
+    To make things simpler, this function gets rid 
+    of the formatting and retains only the intended text.
+
+    See https://api.slack.com/reference/surfaces/formatting#retrieving-messages
+    """
+    return re.sub(r"<(.*?)>", lambda m: m.group(1).split("|")[-1], msg)
